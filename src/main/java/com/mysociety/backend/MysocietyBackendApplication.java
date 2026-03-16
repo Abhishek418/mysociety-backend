64db597f44f700a85969a8e07734f2bd1928c81b
Abhishek418 <abhisheky418@gmail.com>
2026-03-16 20:26:44 +0530

    feat: configure health endpoint and setup environment variable loading

--- src/main/java/com/mysociety/backend/MysocietyBackendApplication.java
+++ src/main/java/com/mysociety/backend/MysocietyBackendApplication.java
@@ -4,12 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
+
 @SpringBootApplication
 @EnableScheduling
 public class MysocietyBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+		
+		dotenv.entries().forEach(entry -> {
+			if (System.getenv(entry.getKey()) == null) {
+				System.setProperty(entry.getKey(), entry.getValue());
+			}
+		});
+
 		SpringApplication.run(MysocietyBackendApplication.class, args);
 	}
-
 }
